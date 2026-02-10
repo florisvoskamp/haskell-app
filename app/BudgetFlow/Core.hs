e6cf9b708e3eebecb35c2bb29ff3f0346649812c
Floris <floris.voskamp@outlook.com>
2026-02-10 19:53:37 +0100

    Add applyEvent function to BudgetFlow.Core for processing Income and Expense events. Update BudgetFlow.Types to define Event and MonthState data types, and modify currency conversion functions to use Money type.

--- app/BudgetFlow/Core.hs
+++ app/BudgetFlow/Core.hs
@@ -1,2 +1,8 @@
 module BudgetFlow.Core where
 
+import BudgetFlow.Types
+
+applyEvent :: Money -> Event -> Money
+applyEvent (Cents balance) (Income (Cents amount)) = Cents (balance + amount)
+applyEvent (Cents balance) (Expense cat (Cents amount)) = Cents (balance - amount)
+
--- app/BudgetFlow/Types.hs
+++ app/BudgetFlow/Types.hs
@@ -3,12 +3,13 @@ module BudgetFlow.Types where
 -- Define types for BudgetFlow
 data Money = Cents Int
 data Category = Category String
-data Events 
+data Event = Income Money | Expense Category Money
+data MonthState = MonthState Int Money
 
 -- Takes 100.00 euro and turns it into 100000 cents
-euroToCents :: Double -> Int
+euroToCents :: Double -> Money
 euroToCents euro = round (euro * 100)
 
 -- Turns cents f.e. 100 into 1.00 euro
-centsToDisplayString :: Int -> String
+centsToDisplayString :: Money -> String
 centsToDisplayString cents = show (fromIntegral cents / 100.0)
