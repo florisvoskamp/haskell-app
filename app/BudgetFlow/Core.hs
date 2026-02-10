f2c1d50776917bbe151c32c53c3613ed81e3bda0
Floris <floris.voskamp@outlook.com>
2026-02-10 20:10:24 +0100

    Add simulateMonth function to BudgetFlow.Core for simulating monthly balance based on a list of events, enhancing financial tracking capabilities.

--- app/BudgetFlow/Core.hs
+++ app/BudgetFlow/Core.hs
@@ -6,3 +6,5 @@ applyEvent :: Money -> Event -> Money
 applyEvent (Cents balance) (Income (Cents amount)) = Cents (balance + amount)
 applyEvent (Cents balance) (Expense cat (Cents amount)) = Cents (balance - amount)
 
+simulateMonth :: Money -> [Event] -> Money
+simulateMonth start events = foldl balance event 
