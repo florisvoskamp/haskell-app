3e5feee1337fa4f3780715b57bfb53730f682fe4
Floris <floris.voskamp@outlook.com>
2026-02-27 11:24:36 +0100

    Add test case for centsToDisplayString in Spec.hs to enhance coverage of BudgetFlow.Types
    - Added a new test case to verify the centsToDisplayString function, ensuring correct conversion of Cents to a display string format.

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,7 +7,8 @@ import BudgetFlow.Types
 main :: IO ()
 main = defaultMain $ testGroup "BudgetFlow"
   [ testGroup "Types" [
-    testCase "euroToCents" $ assertEqual "" (Cents 1250) (euroToCents 12.50)
+    testCase "euroToCents" $ assertEqual "" (Cents 1250) (euroToCents 12.50),
+    testCase "centsToDisplayString" $ assertEqual "" "12.5" (centsToDisplayString (Cents 1250))
   ]
   , testGroup "Core" [
 
