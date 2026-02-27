02d42383a676b6b0cb5f5e7ec4acf93ecf37c495
Floris <floris.voskamp@outlook.com>
2026-02-27 11:41:05 +0100

    Enhance test coverage in Spec.hs by refining applyEvent and euroToCents test cases
    - Updated the test case for euroToCents to include a descriptive label.
    - Added additional test cases for applyEvent to validate both Income and Expense scenarios, ensuring accurate balance calculations.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,16 +2,22 @@ module Main where
 
 import Test.Tasty
 import Test.Tasty.HUnit (testCase, assertEqual)
-import BudgetFlow.Types 
+import BudgetFlow.Types
+    ( Event(Expense, Income),
+      Money(Cents),
+      euroToCents,
+      centsToDisplayString, Category (..) )
+import BudgetFlow.Core ( applyEvent ) 
 
 main :: IO ()
 main = defaultMain $ testGroup "BudgetFlow"
   [ testGroup "Types" [
-    testCase "euroToCents" $ assertEqual "" (Cents 1250) (euroToCents 12.50),
+    testCase "euroToCents" $ assertEqual "Test" (Cents 1250) (euroToCents 12.50),
     testCase "centsToDisplayString" $ assertEqual "" "12.5" (centsToDisplayString (Cents 1250))
   ]
   , testGroup "Core" [
-    testCase "applyEvent" $ assertEqual "" (Cents 1000) (applyEvent (Cents 1000) (Event )
+    testCase "applyEvent" $ assertEqual "Income adds to balance" (Cents 1200) (applyEvent (Cents 1000) (Income (Cents 200))),
+    testCase "applyEvent" $ assertEqual "Expense takes from balance" (Cents 800) (applyEvent (Cents 1000) (Expense (Category "") (Cents 200)))
   ]
   , testGroup "Rules" [
 
